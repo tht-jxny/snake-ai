9d80a9120fe7460d871eb0b63884ceafc6792b15
J W <jbwa@posteo.de>
2018-12-04 20:25:39 +0100

    fixed bugs

--- ga.py
+++ ga.py
@@ -27,29 +27,10 @@ def getInputArray(snake,width,height):
     finalArray += finalArray2
     xFoodCoord = snake.food[0]
     yFoodCoord = snake.food[1]
-    finalArray2 = [xFood,Coord,yFoodCoord]
+    finalArray2 = [xFoodCoord,yFoodCoord]
     finalArray += finalArray2
     for i in range(len(finalArray)):
         finalArray[i] /= 1000.0
         if finalArray[i] == 0.0:
             finalArray[i] = 0.001
     return finalArray
-
-#
-# def nextGeneration():
-#     global savedSnakes,snakes,populationSize,snake
-#     print("New Generation")
-#     calcFitness(savedSnakes)
-#     for i in range(populationSize):
-#         snakes.append(snake())
-#     return snakes
-#
-#
-#
-# def calcFitness(savedSnakes):
-#     global snakes,populationSize,snake
-#     sum = 0
-#     for snake in savedSnakes:
-#         sum += snake.score
-#     for snake in snakes:
-#         snake.fitness = snake.score / sum
